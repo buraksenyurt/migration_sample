a96708b38bfb49a94f6a425479aba36613499d03
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-22 23:03:00 +0300

    DI Modülü eklendi

---
+++ ClassicGames/ClassicGames.DAL/ClassicGamesDBModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+namespace ClassicGames.DAL
+{
+    public class ClassicGamesDBModule
+        :Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CommodoreDBContext>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<GameRepository>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
