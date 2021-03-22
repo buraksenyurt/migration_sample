fb676f92bd9a344f6b80ed5da34d9302728309cc
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-22 23:25:36 +0300

    Oyun fotoğrafı yükleyen sınıf eklendi

---
+++ ClassicGames/ClassicGames.DAL/ImageLoader.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using System.IO;
+using System.Reflection;
+
+namespace ClassicGames.DAL
+{
+    public static class ImageLoader
+    {
+        public static byte[] GetGameCover(string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream(assembly.GetName().Name + ".Assets." + fileName);
+            if (stream != null)
+            {
+                BinaryReader reader = new BinaryReader(stream);
+                byte[] content = new byte[stream.Length];
+                stream.Read(content, 0, content.Length);
+                reader.Close();
+                return content;
+            }
+            else
+            {
+                Log.Warning("{0} isimli resource dosyası bulunamadı.", fileName);
+                return null;
+            }
+        }
+    }
+}
