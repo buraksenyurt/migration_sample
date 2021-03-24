04b621dbeb41ee8e49129e203ec20ac1fa3d9945
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-24 22:36:30 +0300

    DI mekanizması için kod eklendi.

--- ClassicGames/WebClient/Global.asax.cs
+++ ClassicGames/WebClient/Global.asax.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using ClassicGames.DAL;
+using Serilog;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +16,25 @@ namespace WebClient
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ConfigureDI();
+        }
+
+        private static void ConfigureDI()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterModule(new ClassicGamesDBModule());
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        protected void Application_Error()
+        {
+            var exc = Server.GetLastError();
+            Log.Error(exc, $"Hata oluştu.{exc.Message}");
         }
     }
 }
