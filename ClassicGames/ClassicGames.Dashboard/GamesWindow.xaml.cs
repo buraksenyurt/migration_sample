e0333d55870bbee2089cdd2473eb85661dcb33e1
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-27 00:17:14 +0300

    Fotoğraf yükleme operasyonu eklendi.

--- ClassicGames/ClassicGames.Dashboard/GamesWindow.xaml.cs
+++ ClassicGames/ClassicGames.Dashboard/GamesWindow.xaml.cs
@@ -10,9 +10,11 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.IO;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace ClassicGames.Dashboard
 {
@@ -50,7 +52,44 @@ namespace ClassicGames.Dashboard
 
         private void buttonBrowse_Click(object sender, RoutedEventArgs e)
         {
+            // Oyunun kapak fotoğrafını güncellemek veya yeni bir oyun için yenisin eklemek istediğimizde devreye giren olay metodu
+            
+            // Win32 üstünden File Dialog kullanıyoruz
+            var dialog = new OpenFileDialog
+            {
+                Title = "Choose Cover Image",
+                Filter = "JPEG (*.jpg;*.jpeg;*.jpe)|*.jpg;*.jpeg;*.jpe|PNG (*.png)|*.png|All Files (*.*)|*.*"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                // Eğer var olan bir oyun üstünden buraya geldiysek güncelleme söz konusu olacaktır
+                if (((FrameworkElement)sender).DataContext is Game game && game.Id > 0)
+                {
+                    game.Photo = GetPhoto(dialog.FileName);
+                    _gameRepository.UpsertGame(game);
 
+                    GetAllGames();
+                    MessageBox.Show("Kapak fotoğrafı yüklendi!", "Kapak Fotoğrafı", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+                else
+                {
+                    MessageBox.Show("Fotoğrafı yükleyemedim yaa. Sanırım önce bir oyun girmen lazım. :(", "Oyun Ekle", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+            }
+        }
+
+        private byte[] GetPhoto(string filePath)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    byte[] photo = reader.ReadBytes((int)stream.Length);
+                    return photo;
+                }
+            }
         }
 
         private void buttonExport_Click(object sender, RoutedEventArgs e)
