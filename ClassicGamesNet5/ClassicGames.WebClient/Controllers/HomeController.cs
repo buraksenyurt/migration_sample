f85d0618d6ddb2a85594fe38d9201f02015161ae
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-28 15:59:06 +0300

    Controller tipleri düzenlendi

--- ClassicGamesNet5/ClassicGames.WebClient/Controllers/HomeController.cs
+++ ClassicGamesNet5/ClassicGames.WebClient/Controllers/HomeController.cs
@@ -1,37 +1,31 @@
-﻿using ClassicGames.WebClient.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Web;
+//using System.Web.Mvc; //Eski
+using Microsoft.AspNetCore.Mvc; //Yeni
 
 namespace ClassicGames.WebClient.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult About()
         {
+            ViewBag.Message = "Your application description page.";
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public ActionResult Contact()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
