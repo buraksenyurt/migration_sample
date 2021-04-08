bc0150dccade08ca57f8f30baf6dbd2763b48836
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-08 21:06:00 +0300

    DI için Startup sınıf eklendi.

---
+++ ClassicGamesAzure/Analysis/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Analysis;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+/*
+ Assembly çalıştırıldığında Startup sınıfının servis kayıt işlemleri için çağırılmasını garanti ediyoruz. 
+ */
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Analysis
+{
+    public class Startup
+        :FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Servisimizi kayıt ettik. Senaryoda bir servis var ama daha fazla sayıda servis de olabilirdi
+            // DI'ı sevin :D
+            builder.Services.AddSingleton<IAlienistService, AlienistService>();
+        }
+    }
+}
