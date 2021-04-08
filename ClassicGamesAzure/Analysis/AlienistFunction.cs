9ee4849e76727a25fb6ed18e40e344a6594253dd
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-04-08 21:38:11 +0300

    Run metodu, duygu analizi servisini kullanacak şekilde düzenlendi.

--- ClassicGamesAzure/Analysis/AlienistFunction.cs
+++ ClassicGamesAzure/Analysis/AlienistFunction.cs
@@ -1,35 +1,48 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Analysis
 {
-    public static class AlienistFunction
+    public class AlienistFunction
     {
+        private readonly IAlienistService _alienistService;
+        // AlienistService'i constructor üstünden enjekte ediyoruz
+        public AlienistFunction(IAlienistService alienistService)
+        {
+            _alienistService = alienistService;
+        }
         [FunctionName("AlienistFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Yorum için duygu durum analizi fonksiyonu başlatılıyor.");
 
-            string name = req.Query["name"];
+            // QueryString'teki content değerini alıyoruz. Bunu fonksiyonu URL'den kolayca test etmek için tutuyoruz.
+            string content = req.Query["content"];
 
+            /*
+                Birde Body ile gelen içeriği JSON formatında alıyoruz.
+                Yorum ifadesi büyük bir veri olabileceğinden body ile gelmesi çok daha doğru.
+            */
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            content ??= data?.commentText; // body doluysa oradaki commentText değerini alıyoruz
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            /*
+                İster QueryString üstünden test için ister asıl hedefimize uygun olarak Body ile gelsin
+                elimizde bir content olduğunu varsayalım.
+                Bunu servisimize gönderip analiz ettiriyor ve cevabı geriye yolluyoruz.
+            */
+            var result = _alienistService.GetReport(content);
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(result);
         }
     }
 }
